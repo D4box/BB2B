8c6694399c448c2a555926e944cf03b9c952dad3
D^4_box <47934477+D4box@users.noreply.github.com>
2022-07-22 16:37:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+// ==UserScript==
+// @name		BB2B
+// @version		0.1
+// @description	bilibili赛事竞猜辅助插件
+// @icon        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgA…brPPO8tXK68P3/6mBfwFbUQFv/3QxMgAAAABJRU5ErkJggg==
+// @match		http*://www.bilibili.com/v/game/match/*
+// @run-at      document-end
+// ==/UserScript==
+
 (function(){
     'use strict';
-    console.log('网页加载中..');
 })();
 window.onload = function(){
-    console.log('网页加载完成1..');
     setTimeout(function(){
-        console.log('网页加载完成..');
         if (location.href == 'https://www.bilibili.com/v/game/match/competition'){
             document.getElementsByClassName('time-contain')[0].style.cssText = 'background-color:white'
             var sshi = $(".competition-list");
@@ -20,20 +26,17 @@ window.onload = function(){
                 if (sshi[0].children[i].getElementsByClassName('competition-option-content clear-fix')[0].className != 'competition-option-content clear-fix competition-option-in' && sshi[0].children[i]. getElementsByClassName('competition-option-content clear-fix')[1].className != 'competition-option-content clear-fix competition-option-in'){yt = 0}
                 else{yt = 1}
                 if (parseInt(team1) - parseInt(team2) >= 0.5){
-                    console.log(team1+':'+team2+' team1 win');
                     t1c = "font-family:'xx-bin';font-size:27px;color:#222222;letter-spacing:0;line-height:24px;height:24px;width:50px;text-align:center"
                     t2c = "font-family:'xx-bin';font-size:27px;color:#f25d8e;letter-spacing:0;line-height:24px;height:24px;width:50px;text-align:center"
                     if (yt == 0){sshi[0].children[i].getElementsByClassName('competition-option-content clear-fix')[1].style.cssText = 'border: 1px solid #fb7299;border-left-width: 4px'}
                 }
                 else if (parseInt(team1) - parseInt(team2) <= -0.5){
-                    console.log(team1+':'+team2+' team2 win');
                     t1c = "font-family:'xx-bin';font-size:27px;color:#f25d8e;letter-spacing:0;line-height:24px;height:24px;width:50px;text-align:center"
                     t2c = "font-family:'xx-bin';font-size:27px;color:#222222;letter-spacing:0;line-height:24px;height:24px;width:50px;text-align:center"
                     sshi[0].children[i].getElementsByClassName('competition-option-content clear-fix')[0].style.cssText = 'border: 1px solid #fb7299;border-left-width: 4px'
                     if (yt == 0){sshi[0].children[i].getElementsByClassName('competition-option-content clear-fix')[0].style.cssText = 'border: 1px solid #fb7299;border-left-width: 4px'}
                 }
                 else{
-                    console.log(team1+':'+team2+' 难判断');
                     t1c = "font-family:'xx-bin';font-size:27px;color:#222222;letter-spacing:0;line-height:24px;height:24px;width:50px;text-align:center"
                     t2c = "font-family:'xx-bin';font-size:27px;color:#222222;letter-spacing:0;line-height:24px;height:24px;width:50px;text-align:center"
                 }
